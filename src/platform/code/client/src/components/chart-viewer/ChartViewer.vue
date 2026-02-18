5e621fb7e0b43d62f8fd277d7c1705c7381d1cef
patrimato <patricia.mato.miragaya@udc.es>
2026-02-18 21:57:50 +0100

    Make chart axes dynamic based on TSV headers

--- src/platform/code/client/src/components/chart-viewer/ChartViewer.vue
+++ src/platform/code/client/src/components/chart-viewer/ChartViewer.vue
@@ -25,7 +25,7 @@
 
           <v-card-text>
             <v-row justify="center" class="mb-4">
-              <v-col cols="12" md="6">
+              <v-col cols="12" md="4">
                 <v-select
                   v-model="selectedChart"
                   :items="charts"
@@ -35,15 +35,32 @@
                   @change="loadChart"
                 />
               </v-col>
+
+              <v-col cols="12" md="4" v-if="fields.length">
+                <v-select
+                  v-model="selectedX"
+                  :items="fields"
+                  label="Eje X (Categoría)"
+                  @change="renderChart"
+                />
+              </v-col>
+              <v-col cols="12" md="4" v-if="fields.length">
+                <v-select
+                  v-model="selectedY"
+                  :items="fields"
+                  label="Eje Y (Valor)"
+                  @change="renderChart"
+                />
+              </v-col>
             </v-row>
 
             <v-row justify="center" v-if="loading">
-              <v-progress-circular indeterminate />
+              <v-progress-circular indeterminate color="primary" />
             </v-row>
 
-            <v-row v-if="spec">
+            <v-row v-show="!loading && spec">
               <v-col cols="12">
-                <div ref="vegaContainer" />
+                <div ref="vegaContainer" class="d-flex justify-center" />
               </v-col>
             </v-row>
           </v-card-text>
@@ -71,50 +88,70 @@ try {
 
 export default {
   name: "ChartViewer",
-
+  
   data() {
     return {
       charts,
       selectedChart: charts.length ? charts[0].value : null,
       loading: false,
-      spec: null
+      spec: null,
+      fields: [], // TSV headers
+      selectedX: null,
+      selectedY: null
     };
   },
-
   mounted() {
-    if (this.charts.length) {
-      this.selectedChart = this.charts[0].value;
-      this.loadChart();
-    }
+    if (this.selectedChart) this.loadChart();
   },
-
   methods: {
     async loadChart() {
       if (!this.selectedChart) return;
-
       this.loading = true;
-      this.spec = null;
-
+      
       try {
         this.spec = context(this.selectedChart);
 
-        this.$nextTick(async () => {
-          this.$refs.vegaContainer.innerHTML = "";
-          await embed(this.$refs.vegaContainer, this.spec, { actions: false });
-        });
+        const tsvUrl = this.spec.data[0].url;
+        const response = await fetch(tsvUrl);
+        const text = await response.text();
+        const firstLine = text.split('\n')[0];
+        this.fields = firstLine.split('\t').map(f => f.trim());
+
+        this.selectedX = this.fields[0];
+        this.selectedY = this.fields[1] || this.fields[0];
+
+        this.renderChart();
       } catch (err) {
-        console.error("Error loading Vega chart:", err);
+        console.error("Error initializing chart:", err);
       } finally {
         this.loading = false;
       }
     },
-    goBack() {
-      this.$router.push({
-        name: "MapViewer",
-        params: { 
-          backAction: true 
+
+    renderChart() {
+      this.$nextTick(async () => {
+        if (!this.$refs.vegaContainer || !this.spec) return;
+
+        let specString = JSON.stringify(this.spec);
+        specString = specString.replace(/__XFIELD__/g, this.selectedX);
+        specString = specString.replace(/__YFIELD__/g, this.selectedY);
+        
+        const finalSpec = JSON.parse(specString);
+
+        try {
+          await embed(this.$refs.vegaContainer, finalSpec, { 
+            actions: false,
+            renderer: 'svg',
+            width: this.$refs.vegaContainer.offsetWidth * 0.8
+          });
+        } catch (e) {
+          console.error("Vega Embed Error:", e);
         }
       });
+    },
+
+    goBack() {
+      this.$router.push({ name: "MapViewer", params: { backAction: true } });
     }
   }
 };
