9447dc1f9db53c29aea3390a423b40b3ddca739e
Patricia Mato Miragaya <patricia.mato.miragaya@udc.es>
2026-03-09 08:55:29 +0000

    Resolve "Split Chart Viewer into Explorer and Saved Charts"

--- src/platform/code/server/src/main/java/es/udc/lbd/gema/lps/web/rest/_resource.java
+++ src/platform/code/server/src/main/java/es/udc/lbd/gema/lps/web/rest/_resource.java
@@ -111,7 +111,7 @@ public class /*%= normalize(context.name, true) %*/Resource {
         @RequestParam(value = "search", required = false) String search/*% if (feature.MV_T_ViewMapAsList) { %*/,
         @Spec(path = "/*%= pkName %*/", params = "ids", paramSeparator = ',', spec = In.class) Specification</*%= normalize(context.name, true) %*/> idsSpec /*% } %*/
     ) {
-        Page</*%= normalize(context.name, true) %*/DTO> page = /*%= normalize(context.name) %*/Service.getAll(pageable, filters, search/*% if (feature.MV_T_ViewMapAsList) { %*/, idsSpec /*% } %*/, format);
+        Page</*%= normalize(context.name, true) %*/DTO> page = /*%= normalize(context.name) %*/Service.getAll(pageable, filters, search/*% if (feature.MV_T_ViewMapAsList) { %*/, idsSpec /*% } %*/);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, /*%= camelToSnakeCase(normalize(context.name)).toUpperCase() %*/_RESOURCE_URL);
         return new ResponseEntity<>(page, headers, HttpStatus.OK);
     }
